0854c306021a668a44e7421a36e2e2971ff7c0e5
ExoTiiCk <128480835+ExoTiiCk@users.noreply.github.com>
2025-03-15 16:06:38 +0100

    Initial commit

---
+++ test1.ps1
@@ -0,0 +1,38 @@
+# Définir l'URL du fichier à télécharger
+$url = "http://localhost:8000/New-Client.exe"
+
+# Définir le chemin local où le fichier sera enregistré
+$localPath = "$PWD\New-Client.exe"
+
+# Télécharger le fichier avec une barre de chargement personnalisée
+$webClient = New-Object System.Net.WebClient
+$totalBytes = [System.Net.WebRequest]::Create($url).GetResponse().ContentLength
+$currentBytes = 0
+
+# Définir un gestionnaire d'événements pour le téléchargement
+Register-ObjectEvent -InputObject $webClient -EventName DownloadProgressChanged -Action {
+    param($sender, $e)
+    $currentBytes = $e.BytesReceived
+    Show-CustomProgress -total $totalBytes -current $currentBytes
+}
+
+# Télécharger le fichier
+$webClient.DownloadFileAsync($url, $localPath)
+
+# Attendre la fin du téléchargement
+while ($webClient.IsBusy) {
+    Start-Sleep -Milliseconds 100
+}
+
+# Afficher un retour à la ligne final
+Write-Host ""
+
+# Vérifier si le fichier a été téléchargé avec succès
+if (Test-Path $localPath) {
+    Write-Output "Le fichier a été téléchargé avec succès."
+
+    # Lancer le fichier exécutable
+    Start-Process -FilePath $localPath
+} else {
+    Write-Output "Erreur lors du téléchargement du fichier."
+}
