522ddfb40681ba9618f16ca3e74237fab37dff58
ExoTiiCk <128480835+ExoTiiCk@users.noreply.github.com>
2025-03-15 20:35:14 +0100

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -12,38 +12,25 @@ function Show-FileLocation {
     param (
         [string]$filePath
     )
-    Write-Output "Le fichier a été téléchargé à l'emplacement suivant : $filePath"
 }
 
 # Afficher le chemin de téléchargement
-Write-Output "Chemin de téléchargement : $localPath"
 
 # Créer le répertoire "Téléchargements" s'il n'existe pas
 if (-Not (Test-Path -Path "$userProfile\Downloads")) {
     New-Item -ItemType Directory -Path "$userProfile\Downloads"
-    Write-Output "Le répertoire 'Téléchargements' a été créé."
 }
 
 # Télécharger le fichier avec Invoke-WebRequest
 try {
     Invoke-WebRequest -Uri $url -OutFile $localPath
-    Write-Output "Le fichier a été téléchargé avec succès."
     Show-FileLocation -filePath $localPath
-} catch {
-    Write-Output "Erreur lors du téléchargement du fichier : $_"
 }
 
 # Vérifier si le fichier a été téléchargé avec succès
 if (Test-Path $localPath) {
-    Write-Output "Le fichier existe à l'emplacement : $localPath"
 
     # Lancer le fichier exécutable
     try {
         Start-Process -FilePath $localPath
-        Write-Output "Le fichier a été exécuté avec succès."
-    } catch {
-        Write-Output "Erreur lors de l'exécution du fichier : $_"
-    }
-} else {
-    Write-Output "Le fichier n'a pas été téléchargé correctement."
 }
